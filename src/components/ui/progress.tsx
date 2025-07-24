235e391310a36d4ea5ba4a6ca3492f28a7ecb699
doye <edyogoba23@gmail.com>
2025-07-25 00:19:46 +0100

    adjusted current player health UI

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -14,14 +14,14 @@ function Progress({
     <ProgressPrimitive.Root
       data-slot="progress"
       className={cn(
-        "bg-neutral-900/20 relative h-2 w-full overflow-hidden rounded-full dark:bg-neutral-50/20",
+        "bg-[#2B2F1E] relative h-2 w-full overflow-hidden rounded-full dark:bg-[#2B2F1E]",
         className
       )}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="bg-neutral-900 h-full w-full flex-1 transition-all dark:bg-neutral-50"
+        className="bg-[#BFE528] h-full w-full flex-1 transition-all dark:bg-[#BFE528]"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
